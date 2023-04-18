25f56cd2c505125c80e46b61da4852bebf169217
moatazzanwarr <moatazzanwarr@gmail.com>
2023-04-18 17:57:11 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -17,6 +17,10 @@ document.addEventListener("DOMContentLoaded",() => {
     add.onclick = () => {
         const div = document.createElement("div");
         div.setAttribute("class","todo d-flex justify-content-between align-items-center gap-2");
+        const check = document.createElement("input");
+        check.setAttribute("type","checkbox");
+        check.setAttribute("class", "form-check-input");
+        div.appendChild(check);
         const p = document.createElement("p");
         const span = document.createElement("span");
         span.setAttribute("class", "icon-trash-o");
@@ -26,6 +30,14 @@ document.addEventListener("DOMContentLoaded",() => {
         div.appendChild(span)
         data.value = "";
         add.disabled = true;
+        check.addEventListener("click",() => {
+            if(check.checked){
+                p.style.textDecoration = "line-through";
+            }else{
+                p.style.textDecoration = "none";
+
+            }
+        });
         span.onclick = () => {
             span.parentElement.remove();
         }
